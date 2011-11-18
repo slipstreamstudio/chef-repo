a680365be87ec7ff33f30116892bde4ef639080c
Lawrence Curtis <lawrence@slipstreamstudio.com>
2011-11-18 22:36:28 +0000

    changed sources

---
+++ cookbooks/mencoder/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Slipstream Studio"
+maintainer_email "lawrence@slipstreamstudio.com"
+license          "All rights reserved"
+description      "Installs/Configures mencoder"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
---
+++ cookbooks/mencoder/recipes/default.rb
@@ -0,0 +1,3 @@
+package "mencoder" do
+  action :install
+end
